f98ab7dd8cd9dfbca37265edc93437b6e1320190
kasun939 <kmathugama@gmail.com>
2017-04-04 15:29:55 +0530

    html converted

---
+++ PHP/Common/header.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Radius - Coaching and Business HTML Template | Homepage One</title>
+<!-- Stylesheets -->
+<link href="/IHS/Res/css/bootstrap.css" rel="stylesheet">
+<link href="/IHS/Res/css/revolution-slider.css" rel="stylesheet">
+<link href="/IHS/Res/css/style.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="/IHS/Res/css/st.css">
+<!--Favicon-->
+<link rel="shortcut icon" href="/IHS/Res/images/favicon.ico" type="image/x-icon">
+<link rel="icon" href="../Res/images/favicon.ico" type="image/x-icon">
+<!-- Responsive -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+<link href="/IHS/Res/css/responsive.css" rel="stylesheet">
+<link href="/IHS/Res/css/st.css" rel="stylesheet" type="text/css" >
+<!--[if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script><![endif]-->
+<!--[if lt IE 9]><script src="js/respond.js"></script><![endif]-->
+</head>
+
+<body>
+<div class="page-wrapper">
+ 	
+    <!-- Preloader -->
+    <div class="preloader"></div>
+ 	
+    <!-- Main Header / Header Style One-->
+    <header class="main-header header-style-one">
+        
+        <!--Header Top-->
+        
+        
+    	<!--Header-Upper-->
+        <div class="header-upper">
+        	<div class="auto-container">
+            	<div class="clearfix">
+                	
+                	<div class="logo-outer">
+                    	<div class="logo"><a href="index.php"><img src="/IHS/Res/images/logo.png" alt="Radius" title="Radius"></a></div>
+                    </div>
+                    
+                    <!--Top Right-->
+                    <div class="top-right">
+                        <ul class="top-nav clearfix">
+                            <li><a href="signIn.php" style="margin-top: 7px;">Sign In</a></li>
+                            <li><a href="signup.php" style="margin-top: 7px;">Sign Up</a></li>
+                            <!--li><a href="#" style="margin-top: 7px;">Privacy</a></li-->
+                            <li><a href="#" style="margin-top: 7px;">Support</a></li>
+                        </ul>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        
+        <!--Header-Lower-->
+        <div class="header-lower">
+            
+        	<div class="auto-container">
+            	<div class="nav-outer clearfix">
+                    <!-- Main Menu -->
+                    <nav class="main-menu">
+                        <div class="navbar-header">
+                            <!-- Toggle Button -->    	
+                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            </button>
+                        </div>
+                        
+                        <div class="navbar-collapse collapse clearfix">
+                            <ul class="navigation clearfix">
+                                <li><a href="/IHS/Views/index.php">Home</a></li>
+                                <li><a href="#">About Us</a>
+                                	<!--ul>
+                                    	<li><a href="about-us.html">About Us</a></li>
+                                    	<li><a href="team.html">Our Team</a></li>
+                                        <li><a href="what-we-do.html">What We Do</a></li>
+                                    </ul-->
+                                </li>
+                                
+                                <li><a href="services.html">Services</a></li>
+                                <!--li class="dropdown"><a href="#">Pages</a>
+                                    <ul>
+                                    	<li><a href="what-we-do.html">What We Do</a></li>
+                                        <li><a href="project.html">Project</a></li>
+                                        <li><a href="project-detail.html">Project Detail</a></li>
+                                    </ul>
+                                </li-->
+                                <!--li class="dropdown"><a href="#">News</a>
+                                    <ul>
+                                        <li><a href="blog-list.html">Blog</a></li>
+                                        <li><a href="blog-single.html">Blog Details</a></li>
+                                    </ul>
+                                </li-->
+                                <li><a href="contact.html">Contact</a></li>
+                            </ul>
+                        </div>
+                    </nav><!-- Main Menu End-->
+                    
+                    <!--Outer Button-->
+                    <div class="outer-btn"><a href="/IHS/Views/Client/evaluation.php" class="theme-btn consultation">Evaluation Form</a></div>
+                    
+                </div>
+            </div>
+        </div>
+        
+        
+        <!--Sticky Header-->
+        <div class="sticky-header">
+        	<div class="auto-container clearfix">
+            	<!--Logo-->
+            	<div class="logo pull-left">
+                	<a href="index.html" class="img-responsive"><img src="/IHS/Res/images/logo-small.png" alt="Radius" title="Radius"></a>
+                </div>
+                
+                <!--Right Col-->
+                <div class="right-col pull-right">
+                	<!-- Main Menu -->
+                    <nav class="main-menu">
+                        <div class="navbar-header">
+                            <!-- Toggle Button -->    	
+                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            </button>
+                        </div>
+                        
+                        <div class="navbar-collapse collapse clearfix">
+                            <ul class="navigation clearfix">
+                                <li><a href="/IHS/Views/index.php">Home</a></li>
+                                <li><a href="#">About Us</a>
+                                	<!--ul>
+                                    	<li><a href="about-us.html">About Us</a></li>
+                                    	<li><a href="team.html">Our Team</a></li>
+                                        <li><a href="what-we-do.html">What We Do</a></li>
+                                    </ul-->
+                                </li>
+                                <li><a href="services.html">Services</a></li>
+                                <!--li class="dropdown"><a href="#">Pages</a>
+                                    <ul>
+                                    	<li><a href="what-we-do.html">What We Do</a></li>
+                                        <li><a href="project.html">Project</a></li>
+                                        <li><a href="project-detail.html">Project Detail</a></li>
+                                    </ul>
+                                </li-->
+                                <!--li class="dropdown"><a href="#">News</a>
+                                    <ul>
+                                        <li><a href="blog-list.html">Blog</a></li>
+                                        <li><a href="blog-single.html">Blog Details</a></li>
+                                    </ul>
+                                </li-->
+                                <li><a href="contact.html">Contact</a></li>
+                            </ul>
+                        </div>
+                    </nav><!-- Main Menu End-->
+                </div>
+                
+            </div>
+        </div><!--End Sticky Header-->
+    
+    </header>
+    <!--End Main Header -->
--- PHP/index.php
+++ PHP/index.php
@@ -1,6 +1,169 @@
-<?php
-include 'Common/header.php';
-?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Radius - Coaching and Business HTML Template | Homepage One</title>
+<!-- Stylesheets -->
+<link href="/IHS/Res/css/bootstrap.css" rel="stylesheet">
+<link href="/IHS/Res/css/revolution-slider.css" rel="stylesheet">
+<link href="/IHS/Res/css/style.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="/IHS/Res/css/st.css">
+<!--Favicon-->
+<link rel="shortcut icon" href="/IHS/Res/images/favicon.ico" type="image/x-icon">
+<link rel="icon" href="../Res/images/favicon.ico" type="image/x-icon">
+<!-- Responsive -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+<link href="/IHS/Res/css/responsive.css" rel="stylesheet">
+<link href="/IHS/Res/css/st.css" rel="stylesheet" type="text/css" >
+<!--[if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script><![endif]-->
+<!--[if lt IE 9]><script src="js/respond.js"></script><![endif]-->
+</head>
+
+<body>
+<div class="page-wrapper">
+    
+    <!-- Preloader -->
+    <div class="preloader"></div>
+    
+    <!-- Main Header / Header Style One-->
+    <header class="main-header header-style-one">
+        
+        <!--Header Top-->
+        
+        
+        <!--Header-Upper-->
+        <div class="header-upper">
+            <div class="auto-container">
+                <div class="clearfix">
+                    
+                    <div class="logo-outer">
+                        <div class="logo"><a href="index.php"><img src="/IHS/Res/images/logo.png" alt="Radius" title="Radius"></a></div>
+                    </div>
+                    
+                    <!--Top Right-->
+                    <div class="top-right">
+                        <ul class="top-nav clearfix">
+                            <li><a href="signIn.html" style="margin-top: 7px;">Sign In</a></li>
+                            <li><a href="signup.html" style="margin-top: 7px;">Sign Up</a></li>
+                            <!--li><a href="#" style="margin-top: 7px;">Privacy</a></li-->
+                            <li><a href="#" style="margin-top: 7px;">Support</a></li>
+                        </ul>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        
+        <!--Header-Lower-->
+        <div class="header-lower">
+            
+            <div class="auto-container">
+                <div class="nav-outer clearfix">
+                    <!-- Main Menu -->
+                    <nav class="main-menu">
+                        <div class="navbar-header">
+                            <!-- Toggle Button -->      
+                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            </button>
+                        </div>
+                        
+                        <div class="navbar-collapse collapse clearfix">
+                            <ul class="navigation clearfix">
+                                <li><a href="/IHS/Views/index.html">Home</a></li>
+                                <li><a href="#">About Us</a>
+                                    <!--ul>
+                                        <li><a href="about-us.html">About Us</a></li>
+                                        <li><a href="team.html">Our Team</a></li>
+                                        <li><a href="what-we-do.html">What We Do</a></li>
+                                    </ul-->
+                                </li>
+                                
+                                <li><a href="services.html">Services</a></li>
+                                <!--li class="dropdown"><a href="#">Pages</a>
+                                    <ul>
+                                        <li><a href="what-we-do.html">What We Do</a></li>
+                                        <li><a href="project.html">Project</a></li>
+                                        <li><a href="project-detail.html">Project Detail</a></li>
+                                    </ul>
+                                </li-->
+                                <!--li class="dropdown"><a href="#">News</a>
+                                    <ul>
+                                        <li><a href="blog-list.html">Blog</a></li>
+                                        <li><a href="blog-single.html">Blog Details</a></li>
+                                    </ul>
+                                </li-->
+                                <li><a href="contact.html">Contact</a></li>
+                            </ul>
+                        </div>
+                    </nav><!-- Main Menu End-->
+                    
+                    <!--Outer Button-->
+                    <div class="outer-btn"><a href="/IHS/Views/Client/evaluation.html" class="theme-btn consultation">Evaluation Form</a></div>
+                    
+                </div>
+            </div>
+        </div>
+        
+        
+        <!--Sticky Header-->
+        <div class="sticky-header">
+            <div class="auto-container clearfix">
+                <!--Logo-->
+                <div class="logo pull-left">
+                    <a href="index.html" class="img-responsive"><img src="/IHS/Res/images/logo-small.png" alt="Radius" title="Radius"></a>
+                </div>
+                
+                <!--Right Col-->
+                <div class="right-col pull-right">
+                    <!-- Main Menu -->
+                    <nav class="main-menu">
+                        <div class="navbar-header">
+                            <!-- Toggle Button -->      
+                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            </button>
+                        </div>
+                        
+                        <div class="navbar-collapse collapse clearfix">
+                            <ul class="navigation clearfix">
+                                <li><a href="/IHS/Views/index.html">Home</a></li>
+                                <li><a href="#">About Us</a>
+                                    <!--ul>
+                                        <li><a href="about-us.html">About Us</a></li>
+                                        <li><a href="team.html">Our Team</a></li>
+                                        <li><a href="what-we-do.html">What We Do</a></li>
+                                    </ul-->
+                                </li>
+                                <li><a href="services.html">Services</a></li>
+                                <!--li class="dropdown"><a href="#">Pages</a>
+                                    <ul>
+                                        <li><a href="what-we-do.html">What We Do</a></li>
+                                        <li><a href="project.html">Project</a></li>
+                                        <li><a href="project-detail.html">Project Detail</a></li>
+                                    </ul>
+                                </li-->
+                                <!--li class="dropdown"><a href="#">News</a>
+                                    <ul>
+                                        <li><a href="blog-list.html">Blog</a></li>
+                                        <li><a href="blog-single.html">Blog Details</a></li>
+                                    </ul>
+                                </li-->
+                                <li><a href="contact.html">Contact</a></li>
+                            </ul>
+                        </div>
+                    </nav><!-- Main Menu End-->
+                </div>
+                
+            </div>
+        </div><!--End Sticky Header-->
+    
+    </header>
     
     <!--Main Slider-->
     <section class="main-slider" data-start-height="400" data-slide-overlay="yes">
@@ -723,8 +886,8 @@ include 'Common/header.php';
 		<!--Upper Box-->
         
         <div class="upper-box">
-        	<div class="auto-container"
->            	<div class="clearfix">
+        	<div class="auto-container">            	
+                <div class="clearfix">
                 	<div class="pull-left">
                     	<h2>Have any question or need any immigration consultation? </h2>
                     </div>
@@ -839,10 +1002,63 @@ include 'Common/header.php';
                  </div>
              </div>
          </div>
+                  <!--Footer Bottom-->
+         <div class="footer-bottom">
+            <div class="auto-container">
+                <div class="row clearfix">
+                    <div class="col-md-8 col-sm-12 col-xs-12">
+                        <div class="copyright">Copyright <a href="#">Radius</a> &copy; 2017</div>
+                    </div>
+                    <div class="col-md-4 col-sm-12 col-xs-12 text-right">
+                        <ul class="social-icon-one">
+                            <li><a href="#" class="fa fa-facebook"></a></li>
+                            <li><a href="#" class="fa fa-twitter"></a></li>
+                            <li><a href="#" class="fa fa-google-plus"></a></li>
+                            <li><a href="#" class="fa fa-pinterest-p"></a></li>
+                            <li><a href="#" class="fa fa-vimeo"></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+         </div>
 
-<?php
-include 'Common/footer.php';
-?>
+    </footer>
+    
+</div>
+<!--End pagewrapper-->
+
+<!--Scroll to top-->
+<div class="scroll-to-top scroll-to-target" data-target=".main-header"><span class="icon fa fa-long-arrow-up"></span></div>
+
+<script src="/IHS/Res/js/jquery.min.js"></script> 
+<script src="/IHS/Res/js/jquery.js"></script> 
+<script src="/IHS/Res/js/jquery.validate"></script> 
+<script src="/IHS/Res/js/bootstrap.min.js"></script>
+<script src="/IHS/Res/js/revolution.min.js"></script>
+<script src="/IHS/Res/js/jquery.validate.min.js"></script>
+<script src="/IHS/Res/js/additional-methods.min.js"></script>
+<script src="/IHS/Res/js/jquery-ui.js"></script> 
+<script type="text/javascript" src="/IHS/Res/js/jquery.numeric.js"></script>
+<script src="/IHS/Res/js/jquery.fancybox.pack.js"></script>
+<script src="/IHS/Res/js/jquery.fancybox-media.js"></script>
+<script src="/IHS/Res/js/owl.js"></script>
+<script src="/IHS/Res/js/wow.js"></script>
+
+<!--Google Map APi Key-->
+<script src="http://maps.google.com/maps/api/js?key="></script>
+<script src="/IHS/Res/js/map-script.js"></script>
+<!--End Google Map APi-->
+
+<script src="/IHS/Res/js/script.js"></script>
+
+</body>
+</html>
+
+<script>
+        $(document).ready(function(){
+            $("#signinform").validate();
+        });
+</script>
 <script>
 $(document).ready(function(){
     $('[data-toggle="popover"]').popover({html:true}); 
